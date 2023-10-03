2fb418761005351796d74a66cc68da143e8ff45c
Pedro <pedrofigueiraboaviagem@gmail.com>
2023-10-03 19:37:20 -0300

    Adição de todos os caracteres possíveis

--- Modelo para parte 1/main.js
+++ Modelo para parte 1/main.js
@@ -1,52 +1,67 @@
 const inputHTML = document.getElementById('codeEncrypt')
-const inputAHTML = document.getElementById('A')
-const inputBHTML = document.getElementById('B')
+const inputAHTML = document.getElementById('a')
+const inputBHTML = document.getElementById('b')
 const btnEncrypt = document.querySelector('.btnEncrypt')
 const resultHTML = document.querySelector('.result')
 
 // tabela de caracteres para criptografia
 // divididos por positivos e negativos
 const TABLECHARACTER = {
-    positive: {
-        'A': '1', 'B': '2', 'C': '3', 'D': '4', 'E': '5', 'F': '6', 'G': '7', 'H': '8', 'I': '9', 'J': '10',
-        'K': '11', 'L': '12', 'M': '13', 'N': '14', 'O': '15', 'P': '16', 'Q': '17', 'R': '18', 'S': '19', 'T': '20',
-        'U': '21', 'V': '22', 'W': '23', 'X': '24', 'Y': '25', 'Z': '26', '.': '27', ',': '28', ' ': '29'
+    positive = {
+        '\t': '0', ' ': '1', '!': '2', '"': '3', '#': '4', '$': '5', '%': '6', '&': '7', '\'': '8', '(': '9',
+        ')': '10', '*': '11', '+': '12', ',': '13', '-': '14', '.': '15', '/': '16', '0': '17', '1': '18', '2': '19', '3': '20',
+        '4': '21', '5': '22', '6': '23', '7': '24', '8': '25', '9': '26', ':': '27', ';': '28', '<': '29',
+        '=': '30', '>': '31', '?': '32', '@': '33', 'A': '34', 'B': '35', 'C': '36', 'D': '37', 'E': '38', 'F': '39',
+        'G': '40', 'H': '41', 'I': '42', 'J': '43', 'K': '44', 'L': '45', 'M': '46', 'N': '47', 'O': '48',
+        'P': '49', 'Q': '50', 'R': '51', 'S': '52', 'T': '53', 'U': '54', 'V': '55', 'W': '56', 'X': '57', 'Y': '58', 'Z': '59',
+        '[': '60', '/': '61', ']': '62', '^': '63', '_': '64', '`': '65', 'a': '66', 'b': '67', 'c': '68', 'd': '69', 'e': '70',
+        'f': '71', 'g': '72', 'h': '73', 'i': '74', 'j': '75', 'k': '76', 'l': '77', 'm': '78', 'n': '79', 'o': '80', 'p': '81',
+        'q': '82', 'r': '83', 's': '84', 't': '85', 'u': '86', 'v': '87', 'w': '88', 'x': '89', 'y': '90', 'z': '91', '{': '92',
+        '|': '93', '}': '94', '~': '95', 'Ç': '96', 'ü': '97', 'é': '98', 'â': '99', 'ä': '100', 'à': '101', 'å': '102', 'ç': '103',
+        'ê': '104', 'ë': '105', 'è': '106', 'ï': '107', 'î': '108', 'ì': '109', 'Ä': '110', 'Å': '111', 'É': '112', 'æ': '113',
+        'Æ': '114', 'ô': '115', 'ö': '116', 'ò': '117', 'û': '118', 'ù': '119', 'ÿ': '120', 'Ö': '121', 'Ü': '122', '¢': '123',
+        '£': '124', '¥': '125', '₧': '126', 'ƒ': '127', 'á': '128', 'í': '129', 'ó': '130', 'ú': '131', 'ñ': '132', 'Ñ': '133',
+        'ª': '134', 'º': '135', '¿': '136', '⌐': '137', '¬': '138', '½': '139', '¼': '140', '¡': '141', '«': '142', '»': '143',
+        '░': '144', '▒': '145', '▓': '146', '│': '147', '┤': '148', 'Á': '149', 'Â': '150', 'À': '151', '©': '152', '╣': '153',
+        '║': '154', '╗': '155', '╝': '156', '¢': '157', '¥': '158', '┐': '159', '└': '160', '┴': '161', '┬': '162', '├': '163',
+        '─': '164', '┼': '165', 'ã': '166', 'Ã': '167', '╚': '168', '╔': '169', '╩': '170', '╦': '171', '╠': '172', '═': '173',
+        '╬': '174', '¤': '175', 'ð': '176', 'Ð': '177', 'Ê': '178', 'Ë': '179', 'È': '180', 'ı': '181', 'Í': '182', 'Î': '183',
+        'Ï': '184', '┘': '185', '┌': '186', '█': '187', '▄': '188', '¦': '189', 'Ì': '190', '▀': '191', 'Ó': '192', 'ß': '193',
+        'Ô': '194', 'Ò': '195', 'õ': '196', 'Õ': '197', 'µ': '198', 'þ': '199', 'Þ': '200', 'Ú': '201', 'Û': '202', 'Ù': '203',
+        'ý': '204', 'Ý': '205', '¯': '206', '´': '207', '¬': '208', '±': '209', '¦': '210', '§': '211', '÷': '212', '¨': '213',
+        '·': '214', '¸': '215', '¹': '216', '³': '217', '²': '218', '■': '219', '▒': '220', '▓': '221'
     },
-    negative: {
-        'A': '-28', 'B': '-27', 'C': '-26', 'D': '-25', 'E': '-24', 'F': '-23', 'G': '-22', 'H': '-21', 'I': '-20', 'J': '-19',
-        'K': '-18', 'L': '-17', 'M': '-16', 'N': '-15', 'O': '-14', 'P': '-13', 'Q': '-12', 'R': '-11', 'S': '-10', 'T': '-9',
-        'U': '-8', 'V': '-7', 'W': '-6', 'X': '-5', 'Y': '-4', 'Z': '-3', '.': '-2', ',': '-1', ' ': '0'
-    }
+    negative = {
+        '\t': '0', ' ': '-1', '!': '-2', '"': '-3', '#': '-4', '$': '-5', '%': '-6', '&': '-7', '\'': '-8', '(': '-9',
+        ')': '-10', '*': '-11', '+': '-12', ',': '-13', '-': '-14', '.': '-15', '/': '-16', '0': '-17', '1': '-18', '2': '-19', '3': '-20',
+        '4': '-21', '5': '-22', '6': '-23', '7': '-24', '8': '-25', '9': '-26', ':': '-27', ';': '-28', '<': '-29',
+        '=': '-30', '>': '-31', '?': '-32', '@': '-33', 'A': '-34', 'B': '-35', 'C': '-36', 'D': '-37', 'E': '-38', 'F': '-39',
+        'G': '-40', 'H': '-41', 'I': '-42', 'J': '-43', 'K': '-44', 'L': '-45', 'M': '-46', 'N': '-47', 'O': '-48',
+        'P': '-49', 'Q': '-50', 'R': '-51', 'S': '-52', 'T': '-53', 'U': '-54', 'V': '-55', 'W': '-56', 'X': '-57', 'Y': '-58', 'Z': '-59',
+        '[': '-60', '/': '-61', ']': '-62', '^': '-63', '_': '-64', '`': '-65', 'a': '-66', 'b': '-67', 'c': '-68', 'd': '-69', 'e': '-70',
+        'f': '-71', 'g': '-72', 'h': '-73', 'i': '-74', 'j': '-75', 'k': '-76', 'l': '-77', 'm': '-78', 'n': '-79', 'o': '-80', 'p': '-81',
+        'q': '-82', 'r': '-83', 's': '-84', 't': '-85', 'u': '-86', 'v': '-87', 'w': '-88', 'x': '-89', 'y': '-90', 'z': '-91', '{': '-92',
+        '|': '-93', '}': '-94', '~': '-95', 'Ç': '-96', 'ü': '-97', 'é': '-98', 'â': '-99', 'ä': '-100', 'à': '-101', 'å': '-102', 'ç': '-103',
+        'ê': '-104', 'ë': '-105', 'è': '-106', 'ï': '-107', 'î': '-108', 'ì': '-109', 'Ä': '-110', 'Å': '-111', 'É': '-112', 'æ': '-113',
+        'Æ': '-114', 'ô': '-115', 'ö': '-116', 'ò': '-117', 'û': '-118', 'ù': '-119', 'ÿ': '-120', 'Ö': '-121', 'Ü': '-122', '¢': '-123',
+        '£': '-124', '¥': '-125', '₧': '-126', 'ƒ': '-127', 'á': '-128', 'í': '-129', 'ó': '-130', 'ú': '-131', 'ñ': '-132', 'Ñ': '-133',
+        'ª': '-134', 'º': '-135', '¿': '-136', '⌐': '-137', '¬': '-138', '½': '-139', '¼': '-140', '¡': '-141', '«': '-142', '»': '-143',
+        '░': '-144', '▒': '-145', '▓': '-146', '│': '-147', '┤': '-148', 'Á': '-149', 'Â': '-150', 'À': '-151', '©': '-152', '╣': '-153',
+        '║': '-154', '╗': '-155', '╝': '-156', '¢': '-157', '¥': '-158', '┐': '-159', '└': '-160', '┴': '-161', '┬': '-162', '├': '-163',
+        '─': '-164', '┼': '-165', 'ã': '-166', 'Ã': '-167', '╚': '-168', '╔': '-169', '╩': '-170', '╦': '-171', '╠': '-172', '═': '-173',
+        '╬': '-174', '¤': '-175', 'ð': '-176', 'Ð': '-177', 'Ê': '-178', 'Ë': '-179', 'È': '-180', 'ı': '-181', 'Í': '-182', 'Î': '-183',
+        'Ï': '-184', '┘': '-185', '┌': '-186', '█': '-187', '▄': '-188', '¦': '-189', 'Ì': '-190', '▀': '-191', 'Ó': '-192', 'ß': '-193',
+        'Ô': '-194', 'Ò': '-195', 'õ': '-196', 'Õ': '-197', 'µ': '-198', 'þ': '-199', 'Þ': '-200', 'Ú': '-201', 'Û': '-202', 'Ù': '-203',
+        'ý': '-204', 'Ý': '-205', '¯': '-206', '´': '-207', '¬': '-208', '±': '-209', '¦': '-210', '§': '-211', '÷': '-212', '¨': '-213',
+        '·': '-214', '¸': '-215', '¹': '-216', '³': '-217', '²': '-218', '■': '-219', '▒': '-220', '▓': '-221'
+    };
+    
 }
 
 let numberTorDecrypt = []
 
-const encrypt = () => {
-    if (!inputAHTML.value || !inputBHTML.value || !inputHTML.value) return
-
-    // valor vindo do input principal
-    // transformando em array com cada letra
-    // Ex.: ['P', 'O', 'O']
-    const messageToEncryptInChar = inputHTML.value.split('')
-
-    const [wordEncrypt, arrayEncrypt] = encryptMessage(messageToEncryptInChar)
-    numberTorDecrypt = arrayEncrypt
-
-    // adiciona na tela o resultado
-    resultHTML.innerHTML = `<p>Sua mensagem criptografada: <strong>${wordEncrypt}</strong></p>`
-}
-
-const decrypt = () => {
-    if (!inputAHTML.value || !inputBHTML.value || !inputHTML.value) return
-
-    const wordDecrypt = decryptMessage(numberTorDecrypt)
-
-    resultHTML.innerHTML = `<p>Sua mensagem criptografada: <strong>${wordDecrypt}</strong></p>`
-}
-
-
-const encryptMessage = (messageToEncryptInChar) => {
-    let numbersValidsToEncrypt = []
+const encryptMessage = (messageToEncryptInChar) => { //recebe um array de caracteres para criptografar
+    let encryptedMessage = [];
     // pecorre os array com os caracetres
     messageToEncryptInChar.forEach((item) => {
         // cálculo para achar o número da criptografia
@@ -55,154 +70,21 @@ const encryptMessage = (messageToEncryptInChar) => {
         // verifica se está entre o range dos caracteres
         if (number >= 1 && number <= 29) {
             // salva, o char, o numero que está com char 
-            numbersValidsToEncrypt.push({ char: item, number: number, oldNumber: number })
+            encryptedMessage.push(number)
         } else {
             // cálculo um número que esteja entre o range de 1 e 29 (tamanho da tabela)
             const newNumber = findK(number)
-
-            // adiciona o número novo e guarda o antigo tbm
-            // o char ainda não foi atualizado
-            numbersValidsToEncrypt.push({ char: item, number: Number(newNumber), oldNumber: number })
+            encryptedMessage.push(newNumber)
         }
-    })
+        
+    });
 
-    let str = ''
+    const encryptedMessageInChar = encryptedMessage.map((item) => {
 
-    let count = 0
-    // pecorre a quantidade de elementos que seram encriptografado
-    for (count; count < numbersValidsToEncrypt.length; count++) {
-        // se maior que zero usamos a tabela para positivos
-        if (numbersValidsToEncrypt[count].number > 0) {
-            // como a tabela tem números de 1 a 29 
-            // pecorremos pelo index
-            // esse metodo retorn um array com os valores da tabelas (uns números no caso)
-            Object.values(TABLECHARACTER.positive).forEach((item, index) => {
-                // verificamos se o número salvo for igual ao da tabela
-                if (Number(item) === numbersValidsToEncrypt[count].number) {
-                    // adicionamos na string a letra na posição onde o valor foi igual ao número do cálculo da congruenca
-                    str += Object.keys(TABLECHARACTER.positive)[index]
-                }
-            })
-        } else {
-            // mesma lógica para tabela dos negativo
-            Object.values(TABLECHARACTER.negative).forEach((item, index) => {
-                if (Number(item) === numbersValidsToEncrypt[count].number) {
-                    str += Object.keys(TABLECHARACTER.negative)[index]
-                }
-            })
-        }
-    }
-
-    return [str, numbersValidsToEncrypt]
-}
-
-const decryptMessage = (numberTorDecrypt) => {
-    let inverseArray = []
-    let str = ''
-
-    numberTorDecrypt.forEach((item) => {
-        inverseArray.push(inverseFunction(inputAHTML.value, inputBHTML.value, item.oldNumber))
-    })
-
-    inverseArray.forEach((item) => {
-        if (Number(item) > 0) {
-            Object.values(TABLECHARACTER.positive).forEach((tableNumber, index) => {
-                if (Number(tableNumber) === Number(item)) {
-                    str += Object.keys(TABLECHARACTER.positive)[index];
-                }
-            })
-        } else {
-            Object.values(TABLECHARACTER.negative).forEach((tableNumber, index) => {
-                if (Number(tableNumber) === Number(item)) {
-                    str += Object.keys(TABLECHARACTER.negative)[index];
-                }
-            })
-        }
-    })
-
-    return str
-}
-
-// até achar um número enre -28 e 29
-const findK = (number) => {
-    let result
-
-    do {
-        // sorteia um número aleatório enrre -28 e 29
-        const k = Math.floor(Math.random() * 29) - 28
-        // cálculo da conguência
-        result = Number(number) + Number(k * 29)
-    } while (result < -28 || result > 29)
-
-    return Number(result)
-}
-
-// seria a função da inversa
-const inverseFunction = (a, b, x) => {
-    if (x > 0) {
-        return (Number(x) - Number(b)) / Number(a)
-    } else {
-        return (Number(x) + Number(b)) / Number(a)
-    }
-}
-
-function generateFunction() {
-    let a = document.getElementById('a').value;
-    let b = document.getElementById('b').value;
-
-    let valor1 = ``;
-    let valor2 = ``;
-    let operacaoA = ``;
-    let operacaoB = ``;
-    let temA = true;
-    let temB = true;
-
-    if (a > 0) {
-        valor1 = `${a}x`;
-    } else if (a < 0) {
-        valor1 = `${a * -1}x`;
-        operacaoA = `-`;
-    } else if (a == 0) {
-        valor1 = ``;
-        temA = false;
-    }
+    });
 
 
-    if (b > 0 && temA) {
-        valor2 = `${b}`;
-        operacaoB = `+`;
-    } else if (b > 0 && !temA) {
-        valor2 = `${b}`;
-    } else if ((b < 0 && temA) || (b < 0 && !temA)) {
-        valor2 = `${b * -1}`;
-        operacaoB = `-`;
-    } else if (b == 0) {
-        temB = false;
-    }
 
 
-    let result =`<math>
-                    <msub>
-                        <mi>F</mi>
-                        <mi>(x)</mi>
-                    </msub>
-                    <mo>=</mo>
-                    <mo>${operacaoA}</mo>
-                    <mrow>${valor1}</mrow>
-                    <mo>${operacaoB}</mo>
-                    <mrow>${valor2}</mrow>
-                </math>`;
-    
-    if (!temA && !temB) {
-        result =`<math>
-                    <msub>
-                        <mi>F</mi>
-                        <mi>(x)</mi>
-                    </msub>
-                    <mo>=</mo>
-                    <mn>0</mn>
-                </math>`;
-    } 
 
-    document.getElementById('function').innerHTML = result;
 }
